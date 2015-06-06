2d2c908b9e07790a26161d3cdb8f60cf9f07a3bc
Matthew Francis Brunetti <zenflow87@gmail.com>
2015-06-05 22:47:10 -0400

    added shorthand optional argument to end(); added test for that feature; improved existing test

--- test/index.js
+++ test/index.js
@@ -11,14 +11,30 @@ var dummy_events_keys = _.keys(dummy_events);
 test('works :p', function(t){
 	var ee = new EventEmitter;
 	var end = gatherEvents(ee, _.shuffle(dummy_events_keys));
+	dummy_events_keys = _.shuffle(dummy_events_keys);
 	_.forEach(dummy_events_keys, function(event_name){
 		ee.emit(event_name, dummy_events[event_name]);
 	});
 	var events = end();
 	t.ok(events.length==dummy_events_keys.length);
 	for (var i = 0; i < events.length; i++){
-		t.ok(events[i].name==dummy_events_keys[i]);
-		t.ok(events[i].args[0]==dummy_events[dummy_events_keys[i]]);
+		t.deepEqual(events[i], {name: dummy_events_keys[i], args: [dummy_events[dummy_events_keys[i]]]});
+	}
+	t.end();
+});
+test('shorthand option works', function(t){
+	var ee = new EventEmitter;
+	var end = gatherEvents(ee, _.shuffle(dummy_events_keys));
+	dummy_events_keys = _.shuffle(dummy_events_keys);
+	_.forEach(dummy_events_keys, function(event_name){
+		ee.emit(event_name, dummy_events[event_name]);
+	});
+	var events = end(true);
+	t.ok(events.length==dummy_events_keys.length);
+	for (var i = 0; i < events.length; i++){
+		var expected = {};
+		expected[dummy_events_keys[i]] = [dummy_events[dummy_events_keys[i]]];
+		t.deepEqual(events[i], expected);
 	}
 	t.end();
 });
